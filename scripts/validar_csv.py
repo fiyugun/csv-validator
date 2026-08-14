d870c7b3788b00995155d02f0a501044bd3fb24a
Luigui <fiyugun@gmail.com>
2026-08-14 16:32:34 -0500

    Se agregaron mas condiciones

--- scripts/validar_csv.py
+++ scripts/validar_csv.py
@@ -2,10 +2,24 @@ import pandas as pd
 
 df = pd.read_csv("data/clientes.csv")
 
-errores = df[df["saldo"] < 0]
+errorsaldo = df[df["saldo"] < 0]
 
-if len(errores) > 0:
+
+# cliente vacío
+errorvacios = df[df["cliente"].isna()]
+
+# cliente duplicado
+errorduplicados = df[df.duplicated("cliente")]
+
+
+if len(errorsaldo) > 0:
     print("ERROR: Se encontraron saldos negativos")
-    print(errores)
-else:
-    print("Validación exitosa")
+    print(errorsaldo)
+    if len(errorvacios) > 0:
+        print("ERROR: ❌ "+ str(len(errorvacios)) + " clientes vacíos")
+        print(errorvacios)
+        if len(errorduplicados) > 0:
+            print("ERROR: ❌ "+ str(len(errorduplicados)) + " clientes duplicados")
+            print(errorduplicados)
+        else:
+            print("Validación exitosa")
